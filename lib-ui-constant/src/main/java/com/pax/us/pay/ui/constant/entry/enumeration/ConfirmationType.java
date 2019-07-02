c1614ad52363b8f7fdd5e7812ef5941b4fd49fa5
Kim-Linhb <kim.lin@pax.us>
2019-07-02 18:18:30 -0400

    add new enum

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/ConfirmationType.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/ConfirmationType.java
@@ -3,4 +3,8 @@ package com.pax.us.pay.ui.constant.entry.enumeration;
 public final class ConfirmationType {
     public static final String YES = "YES";
     public static final String NO = "NO";
+
+    public static String[] values() {
+        return new String[]{YES, NO};
+    }
 }
---
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/MOTOType.java
@@ -0,0 +1,11 @@
+package com.pax.us.pay.ui.constant.entry.enumeration;
+
+public class MOTOType {
+
+    public static final String MAIL = "Mail Order";
+    public static final String PHONE = "Phone Order";
+
+    public static String[] values() {
+        return new String[]{MAIL, PHONE};
+    }
+}
