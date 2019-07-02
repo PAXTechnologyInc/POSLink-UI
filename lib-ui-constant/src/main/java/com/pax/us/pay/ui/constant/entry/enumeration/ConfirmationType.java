c94f5e82eff6dc8618775455c9558e34d217f794
Jolie-Yanglj <jolie.yang@pax.us>
2019-07-02 16:00:55 -0400

    1. Change file name

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/ConfirmationType.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/ConfirmationType.java
@@ -1,6 +1,6 @@
 package com.pax.us.pay.ui.constant.entry.enumeration;
 
-public final class Confirmation {
+public final class ConfirmationType {
     public static final String YES = "YES";
     public static final String NO = "NO";
 }
