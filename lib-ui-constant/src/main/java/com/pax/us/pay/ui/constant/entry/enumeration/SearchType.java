3e87f984a9492271e5e4e1b394be6cc56362b1b7
Jolie-Yanglj <jolie.yang@pax.us>
2019-07-24 10:32:52 -0400

    1.Add search type.

---
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchType.java
@@ -0,0 +1,14 @@
+package com.pax.us.pay.ui.constant.entry.enumeration;
+
+public class SearchType {
+    public static final String BY_TRANS_NUMBER = "BY TRANS NUMBER";
+    public static final String BY_LAST_4_DIGITS = "BY LAST 4 DIGITS";
+    public static final String BY_INVOICE_NUMBER = "BY INVOICE NUMBER";
+    public static final String BY_CARD_TYP = "BY CARD TYP";
+    public static final String BY_CLERK_SERVER_ID = "BY CLERK SERVER ID ";
+    public static final String BY_UNTIPPED = "SEARCH UNTIPPED ";
+    public static final String BY_STORE_FORWARD = "SEARCH STORE FORWARD ";
+    public static final String BY_UPLOAD_TRANS = "SEARCH UPLOAD TRANSACTION ";
+    public static final String BY_RESENT_FAILED = "SEARCH RESENT FAILED";
+
+}
