a9d3398a2f8b3fa5948154a16a28d70d1d3d098f
Jolie-Yanglj <jolie.yang@pax.us>
2019-07-25 15:04:52 -0400

    1.Add upload parameter for batch status

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/status/StatusData.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/status/StatusData.java
@@ -3,4 +3,8 @@ package com.pax.us.pay.ui.constant.status;
 public final class StatusData {
     public static final String PARAM_CODE = "resultCode";
     public static final String PARAM_MSG = "resultMessage";
+
+    public static final String PARAM_SF_TYPE = "sfType";
+    public static final String PARAM_SF_CURRENT_COUNT = "sfCurrent";
+    public static final String PARAM_SF_TOTAL_COUNT = "sfTotal";
 }
