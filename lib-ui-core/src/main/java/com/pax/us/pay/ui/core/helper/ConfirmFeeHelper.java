ea1c917af4c113d51951de6c07bc65a09148ec20
arvind-pax <arvind.rekhasura@pax.us>
2019-09-25 16:31:23 -0400

    ANBP-352 fix: Added a new ConfirmFeeActivity for displaying fee details in tabular format.

---
+++ lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/ConfirmFeeHelper.java
@@ -0,0 +1,27 @@
+package com.pax.us.pay.ui.core.helper;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.pax.us.pay.ui.constant.entry.EntryRequest;
+import com.pax.us.pay.ui.core.BaseActionHelper;
+import com.pax.us.pay.ui.core.api.IMessageListener;
+import com.pax.us.pay.ui.core.api.IRespStatus;
+import com.pax.us.pay.ui.core.api.IUIListener;
+
+/**
+ * @author arvind 
+ */
+
+public class ConfirmFeeHelper extends BaseActionHelper {
+    public ConfirmFeeHelper(@Nullable IUIListener uiListener, @Nullable IRespStatus respStatus) {
+        super(uiListener, respStatus);
+    }
+
+    public void sendNext(boolean flag) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(EntryRequest.PARAM_CONFIRMED, flag);
+        super.sendNext(bundle);
+    }
+
+}
