640b5680f63b27c17755644c29a773ef77c6a9be
Jolie-Yanglj <jolie.yang@pax.us>
2019-09-27 16:45:21 -0400

    1. Add by reference number for enum search criteria.

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchCriteria.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchCriteria.java
@@ -7,6 +7,7 @@ public class SearchCriteria {
     public static final String BY_CARD_TYPE = "BY CARD TYP";
     public static final String BY_CLERK_SERVER_ID = "BY CLERK SERVER ID";
     public static final String BY_UNTIPPED = "SEARCH UNTIPPED";
+    public static final String BY_REFERENCE_NUMBER = "BY REFERENCE NUMBER";
     public static final String BY_STORE_FORWARD = "SEARCH STORE FORWARD";
     public static final String BY_UPLOAD_TRANS = "SEARCH UPLOAD TRANSACTION";
     public static final String BY_RESENT_FAILED = "SEARCH RESENT FAILED";
