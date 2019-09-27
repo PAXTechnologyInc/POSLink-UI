f6dab73ffb65a5399f83eac078870077f6f8f39c
Jolie-Yanglj <jolie.yang@pax.us>
2019-09-27 16:49:40 -0400

    1. Fixed bug with  name of "BY CARD TYPE" for enum search criteria.

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchCriteria.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchCriteria.java
@@ -4,7 +4,7 @@ public class SearchCriteria {
     public static final String BY_TRANS_NUMBER = "BY TRANS NUMBER";
     public static final String BY_LAST_4_DIGITS = "BY LAST 4 DIGITS";
     public static final String BY_INVOICE_NUMBER = "BY INVOICE NUMBER";
-    public static final String BY_CARD_TYPE = "BY CARD TYP";
+    public static final String BY_CARD_TYPE = "BY CARD TYPE";
     public static final String BY_CLERK_SERVER_ID = "BY CLERK SERVER ID";
     public static final String BY_UNTIPPED = "SEARCH UNTIPPED";
     public static final String BY_REFERENCE_NUMBER = "BY REFERENCE NUMBER";
