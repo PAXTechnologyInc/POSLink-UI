e7c74714481651a5db628e7b34efe8791982c4c0
Jolie-Yanglj <jolie.yang@pax.us>
2020-02-04 15:06:36 -0500

    1. remove display totalAmount processing

--- lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/EnterFleetLicenseNumberHelper.java
+++ lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/EnterFleetLicenseNumberHelper.java
@@ -1,17 +1,12 @@
 package com.pax.us.pay.ui.core.helper;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.pax.us.pay.ui.constant.entry.EntryExtraData;
 import com.pax.us.pay.ui.constant.entry.EntryRequest;
 import com.pax.us.pay.ui.core.BaseActionHelper;
-import com.pax.us.pay.ui.core.api.IAmountListener;
-import com.pax.us.pay.ui.core.api.ICurrencyListener;
 import com.pax.us.pay.ui.core.api.IMessageListener;
 import com.pax.us.pay.ui.core.api.IRespStatus;
-import com.pax.us.pay.ui.core.api.IUIListener;
 
 public class EnterFleetLicenseNumberHelper extends BaseActionHelper {
 
@@ -25,21 +20,7 @@ public class EnterFleetLicenseNumberHelper extends BaseActionHelper {
         super.sendNext(bundle);
     }
 
-    @Override
-    protected void showUI(@Nullable IUIListener uiListener, @NonNull Bundle bundle) {
-        super.showUI(uiListener, bundle);
-
-        if (uiListener instanceof ICurrencyListener) {
-            String currency = bundle.getString(EntryExtraData.PARAM_CURRENCY, "USD");
-            ((ICurrencyListener) uiListener).onShowCurrency(currency, false);
-        }
-
-        if (uiListener instanceof IAmountListener && bundle.containsKey(EntryExtraData.PARAM_TOTAL_AMOUNT)) {
-            ((IAmountListener) uiListener).onShowAmount(bundle.getLong(EntryExtraData.PARAM_TOTAL_AMOUNT));
-        }
-    }
-
-    public interface IEnterFleetLicenseNumberListener extends IMessageListener, ICurrencyListener, IAmountListener {
+    public interface IEnterFleetLicenseNumberListener extends IMessageListener {
     }
 
 }
