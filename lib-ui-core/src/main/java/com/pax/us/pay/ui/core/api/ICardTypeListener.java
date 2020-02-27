51910bcfd5a8f6de30a5c5ad23cdeac34e0097c9
Jolie-Yanglj <jolie.yang@pax.us>
2020-02-27 14:55:05 -0500

    1. Add DISPLAY_APPROVE_MESSAGE actions

---
+++ lib-ui-core/src/main/java/com/pax/us/pay/ui/core/api/ICardTypeListener.java
@@ -0,0 +1,7 @@
+package com.pax.us.pay.ui.core.api;
+
+import android.support.annotation.NonNull;
+
+public interface ICardTypeListener {
+    public void onShowCardType(@NonNull String cardType);
+}
---
+++ lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/DisplayApproveMessageHelper.java
@@ -0,0 +1,40 @@
+package com.pax.us.pay.ui.core.helper;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.pax.us.pay.ui.constant.entry.EntryExtraData;
+import com.pax.us.pay.ui.constant.entry.EntryRequest;
+import com.pax.us.pay.ui.core.BaseActionHelper;
+import com.pax.us.pay.ui.core.api.ICardTypeListener;
+import com.pax.us.pay.ui.core.api.IMessageListener;
+import com.pax.us.pay.ui.core.api.IRespStatus;
+import com.pax.us.pay.ui.core.api.IUIListener;
+
+public class DisplayApproveMessageHelper extends BaseActionHelper {
+    public DisplayApproveMessageHelper(@Nullable IDisplayApproveMessageListener uiListener, @Nullable IRespStatus respStatus) {
+        super(uiListener, respStatus);
+    }
+
+    public void sendNext(boolean flag) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(EntryRequest.PARAM_CONFIRMED, flag);
+        super.sendNext(bundle);
+    }
+
+    @Override
+    protected void showUI(@Nullable IUIListener uiListener, @NonNull Bundle bundle) {
+        super.showUI(uiListener, bundle);
+        if (uiListener instanceof IDisplayApproveMessageListener && bundle.containsKey(EntryExtraData.PARAM_CARD_TYPE)) {
+            String cardType = bundle.getString(EntryExtraData.PARAM_CARD_TYPE);
+            if (!TextUtils.isEmpty(cardType)) {
+                ((ICardTypeListener) uiListener).onShowCardType(cardType);
+            }
+        }
+    }
+
+    public interface IDisplayApproveMessageListener extends IMessageListener, ICardTypeListener {
+    }
+}
