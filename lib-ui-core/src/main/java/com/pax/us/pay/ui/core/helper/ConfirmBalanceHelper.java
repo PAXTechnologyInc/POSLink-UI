17ed666704a8d1ba6aa917094f00e713f08e8ea6
Jolie-Yanglj <jolie.yang@pax.us>
2020-06-04 20:22:47 -0400

    Add support CONFIRM_BALANCE action

---
+++ lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/ConfirmBalanceHelper.java
@@ -0,0 +1,48 @@
+package com.pax.us.pay.ui.core.helper;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.pax.us.pay.ui.constant.entry.EntryExtraData;
+import com.pax.us.pay.ui.constant.entry.EntryRequest;
+import com.pax.us.pay.ui.constant.entry.enumeration.CurrencyType;
+import com.pax.us.pay.ui.core.BaseActionHelper;
+import com.pax.us.pay.ui.core.api.IAmountListener;
+import com.pax.us.pay.ui.core.api.ICurrencyListener;
+import com.pax.us.pay.ui.core.api.IMessageListener;
+import com.pax.us.pay.ui.core.api.IRespStatus;
+import com.pax.us.pay.ui.core.api.ISurchargeFeeNameListener;
+import com.pax.us.pay.ui.core.api.IUIListener;
+
+public class ConfirmBalanceHelper extends BaseActionHelper {
+    public ConfirmBalanceHelper(@Nullable IConFirmBalanceListener uiListener, @Nullable IRespStatus respStatus) {
+        super(uiListener, respStatus);
+    }
+
+    public void sendNext(boolean flag) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(EntryRequest.PARAM_CONFIRMED, flag);
+        super.sendNext(bundle);
+    }
+
+    @Override
+    protected void showUI(@Nullable IUIListener uiListener, @NonNull Bundle bundle) {
+        super.showUI(uiListener, bundle);
+        if ((uiListener instanceof ICurrencyListener) &&
+                (uiListener instanceof IAmountListener && bundle.containsKey(EntryExtraData.PARAM_BALANCE))) {
+            String currency = bundle.getString(EntryExtraData.PARAM_CURRENCY, "USD");
+            if (currency.equals(CurrencyType.POINT))
+                ((ICurrencyListener) uiListener).onShowCurrency(currency, true);
+            else
+                ((ICurrencyListener) uiListener).onShowCurrency(currency, false);
+
+            ((IAmountListener) uiListener).onShowAmount(bundle.getLong(EntryExtraData.PARAM_BALANCE));
+        }
+
+    }
+
+    public interface IConFirmBalanceListener extends IMessageListener, ICurrencyListener, IAmountListener {
+    }
+
+}
