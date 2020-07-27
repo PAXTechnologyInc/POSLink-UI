a89520d51ca01fa1f6c6b31c5a415b530a4b56fa
JasonGuo2020 <jason.guo@pax.us>
2020-07-27 15:03:36 -0400

    Add the currency CAD & GBP;

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/CurrencyType.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/CurrencyType.java
@@ -1,11 +1,13 @@
 package com.pax.us.pay.ui.constant.entry.enumeration;
 
 public class CurrencyType {
+    public static final String CAD = "CAD";
+    public static final String GBP = "GBP";
     public static final String USD = "USD";
     public static final String POINT = "POINT";
 
     public static String[] values() {
-        return new String[]{USD, POINT};
+        return new String[]{CAD, GBP, USD, POINT};
     }
 
 }
