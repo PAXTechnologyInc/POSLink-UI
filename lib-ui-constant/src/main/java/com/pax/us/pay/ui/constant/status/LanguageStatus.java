f726f575d515741ec9025783bfa9946fb04ef6d3
Z.Z. Li <45825870+zzlipax@users.noreply.github.com>
2020-09-10 16:31:24 -0400

    Add LanguageReceiver for supporting multi language

---
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/status/LanguageStatus.java
@@ -0,0 +1,17 @@
+package com.pax.us.pay.ui.constant.status;
+
+/**
+ * Created by ZZ on 9/10/2020
+ */
+public class LanguageStatus {
+    public static final String CATEGORY = "com.pax.us.pay.status.category.LANGUAGE";
+
+    public static final String SET_LANGUAGE = "com.pax.us.pay.SET_LANGUAGE";
+
+    /**
+     * PARAM_LANGUAGE: Extra data in Action SET_LANGUAGE <br>
+     * lowercase 2 to 8 language code.
+     * value type: String <br>
+     */
+    public static final String PARAM_LANGUAGE = "language";
+}
