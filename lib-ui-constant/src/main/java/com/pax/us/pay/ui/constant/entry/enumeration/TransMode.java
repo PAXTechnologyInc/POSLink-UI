33d506902145b5b9093e913c4063e7ce84464434
Jolie-Yanglj <jolie.yang@pax.us>
2020-09-11 15:35:17 -0400

    [ADJ-96]When the test mode is enabled, the UI should has the watermark "Test Mode".

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/TransMode.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/TransMode.java
@@ -3,5 +3,6 @@ package com.pax.us.pay.ui.constant.entry.enumeration;
 public class TransMode {
     public static final String NORMAL = "NORMAL";
     public static final String DEMO = "DEMO";
+    public static final String TEST = "TEST";
 
 }
