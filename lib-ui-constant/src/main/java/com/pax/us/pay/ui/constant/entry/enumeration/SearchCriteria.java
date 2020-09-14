8a76c354bc7d9b6dc4cf8017c1e33eb9eeb2a193
Jolie-Yanglj <jolie.yang@pax.us>
2020-09-14 11:59:10 -0400

    Fixed spelling error for BY_RESENT_FAILED

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchCriteria.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/SearchCriteria.java
@@ -10,6 +10,6 @@ public class SearchCriteria {
     public static final String BY_REFERENCE_NUMBER = "BY REFERENCE NUMBER";
     public static final String BY_STORE_FORWARD = "SEARCH STORE FORWARD";
     public static final String BY_UPLOAD_TRANS = "SEARCH UPLOAD TRANSACTION";
-    public static final String BY_RESENT_FAILED = "SEARCH RESENT FAILED";
+    public static final String BY_RESENT_FAILED = "SEARCH RE-SENT FAILED";
 
 }
