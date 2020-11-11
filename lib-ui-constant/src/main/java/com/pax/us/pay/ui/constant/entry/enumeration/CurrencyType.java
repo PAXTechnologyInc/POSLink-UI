cf01b670058c0813dcd8f578f5ca247129dd91dd
JasonGuo2020 <jason.guo@pax.us>
2020-11-11 09:10:55 -0500

    [WPROI-1]: Support the currency Euro

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/CurrencyType.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/CurrencyType.java
@@ -2,12 +2,13 @@ package com.pax.us.pay.ui.constant.entry.enumeration;
 
 public class CurrencyType {
     public static final String CAD = "CAD";
+    public static final String EUR = "EUR";
     public static final String GBP = "GBP";
     public static final String USD = "USD";
     public static final String POINT = "POINT";
 
     public static String[] values() {
-        return new String[]{CAD, GBP, USD, POINT};
+        return new String[]{CAD, EUR, GBP, USD, POINT};
     }
 
 }
