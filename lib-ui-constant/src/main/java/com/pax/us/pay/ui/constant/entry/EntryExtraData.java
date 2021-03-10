9980d297ddffc3bb797637d1227858b8b27f7eb5
Jolie-Yanglj <jolie.yang@pax.us>
2021-03-10 14:33:03 -0500

    Optimized the code

--- lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/EntryExtraData.java
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/EntryExtraData.java
@@ -10,7 +10,7 @@ public final class EntryExtraData {
     public static final String PARAM_TERMINAL_ID = "terminalID";
     public static final String PARAM_TRANS_MODE = "transMode";
     public static final String PARAM_EDC_TYPE = "edcType";
-    public static final String PARAM_SUB_TRANS_TYPE = "subTransType";
+//    public static final String PARAM_SUB_TRANS_TYPE = "subTransType";
     public static final String PARAM_TRANS_NUMBER = "transNumber";
 
     public static final String PARAM_OPTIONS = "options";
@@ -40,7 +40,8 @@ public final class EntryExtraData {
     public static final String PARAM_INTERFACE_STYLES = "interfaceStyles";
     public static final String PARAM_MERCHANT_NAME = "merchantName";
     public static final String PARAM_SHOW_VIRTUAL_PINPAD = "showVirtualPinPad";
-    public static final String PARAM_HAS_PHYSICAL_KEYBOARD = "hasPhyKeyboard";
+
+//    public static final String PARAM_HAS_PHYSICAL_KEYBOARD = "hasPhyKeyboard";
 
 
 
@@ -77,51 +78,51 @@ public static final String PARAM_BALANCE = "balance";
      * PARAM_TRANS_MODE : transaction mode <br>
      * enum: {@link com.pax.us.pay.ui.constant.entry.enumeration.TransMode}
      */
-    public static final String PARAM_CARD_NUMBER = "cardNumber";
-    public static final String PARAM_BATCH_NUMBER = "batchNumber";
-    public static final String PARAM_ORDER_NUMBER = "orderNumber";
+//    public static final String PARAM_CARD_NUMBER = "cardNumber";
+//    public static final String PARAM_BATCH_NUMBER = "batchNumber";
+//    public static final String PARAM_ORDER_NUMBER = "orderNumber";
     public static final String PARAM_TIP1 = "tip1";
     public static final String PARAM_TIP2 = "tip2";
     public static final String PARAM_TIP3 = "tip3";
     public static final String PARAM_APPROVED_AMOUNT = "approvedAmount";
-    public static final String PARAM_BASE_POINTS = "basePoints";
-    public static final String PARAM_TOTAL_POINTS = "totalPoints";
-    public static final String PARAM_APPROVED_POINTS = "approvedPoints";
-    public static final String PARAM_TAX = "tax";
+//    public static final String PARAM_BASE_POINTS = "basePoints";
+//    public static final String PARAM_TOTAL_POINTS = "totalPoints";
+//    public static final String PARAM_APPROVED_POINTS = "approvedPoints";
+//    public static final String PARAM_TAX = "tax";
     public static final String PARAM_CASH_BACK = "cashBack";
-    public static final String PARAM_MERCHANT_FEE = "merchantFee";
+//    public static final String PARAM_MERCHANT_FEE = "merchantFee";
     public static final String PARAM_SURCHARGE_FEE_NAME = "surchargeFeeName";
     public static final String PARAM_SURCHARGE_FEE = "surchargeFee";
     public static final String PARAM_ADDITIONAL_FEE = "additionalFee";
     public static final String PARAM_ADDITIONAL_FEE_NAME = "additionalFeeName";
-    public static final String PARAM_PRIMARY_AMOUNT = "primaryAmount";
+//    public static final String PARAM_PRIMARY_AMOUNT = "primaryAmount";
 
     public static final String PARAM_FSA_AMOUNT_OPTIONS = "fsaAmountOptions";
-    public static final String PARAM_HEALTH_CARE_AMOUNT = "healthCareAmount";
-    public static final String PARAM_CLINIC_AMOUNT = "clinicAmount";
-    public static final String PARAM_DENTAL_AMOUNT = "dentalAmount";
-    public static final String PARAM_COPAY_AMOUNT = "copayAmount";
-    public static final String PARAM_PRESCRIPTION_AMOUNT = "prescriptionAmount";
-    public static final String PARAM_VISION_AMOUNT = "visionAmount";
-    public static final String PARAM_TRANSIT_AMOUNT = "transitAmount";
+//    public static final String PARAM_HEALTH_CARE_AMOUNT = "healthCareAmount";
+//    public static final String PARAM_CLINIC_AMOUNT = "clinicAmount";
+//    public static final String PARAM_DENTAL_AMOUNT = "dentalAmount";
+//    public static final String PARAM_COPAY_AMOUNT = "copayAmount";
+//    public static final String PARAM_PRESCRIPTION_AMOUNT = "prescriptionAmount";
+//    public static final String PARAM_VISION_AMOUNT = "visionAmount";
+//    public static final String PARAM_TRANSIT_AMOUNT = "transitAmount";
 
-    public static final String PARAM_STATUS = "status";
-    public static final String PARAM_CLERK_ID = "clerkID";
-    public static final String PARAM_CLERK_NAME = "clerkName";
-    public static final String PARAM_INVOICE_NUMBER = "invoiceNumber";
-    public static final String PARAM_PO_NUMBER = "poNumber";
-    public static final String PARAM_TABLE_NUMBER = "tableNumber";
-    public static final String PARAM_GUESTS_NUMBER = "guestsNumber";
-    public static final String PARAM_CUST_CODE = "custCode";
-    public static final String PARAM_MERCHANT_TAX_ID = "merchantTaxID";
-    public static final String PARAM_PROD_DESC = "prodDesc";
-    public static final String PARAM_TAX_EXEMPT_ID = "taxExemptID";
-    public static final String PARAM_REF_NUMBER = "refNumber";
-    public static final String PARAM_AUTH_CODE = "authCode";
-    public static final String PARAM_RESPONSE_STATUS = "responseStatus";
+//    public static final String PARAM_STATUS = "status";
+//    public static final String PARAM_CLERK_ID = "clerkID";
+//    public static final String PARAM_CLERK_NAME = "clerkName";
+//    public static final String PARAM_INVOICE_NUMBER = "invoiceNumber";
+//    public static final String PARAM_PO_NUMBER = "poNumber";
+//    public static final String PARAM_TABLE_NUMBER = "tableNumber";
+//    public static final String PARAM_GUESTS_NUMBER = "guestsNumber";
+//    public static final String PARAM_CUST_CODE = "custCode";
+//    public static final String PARAM_MERCHANT_TAX_ID = "merchantTaxID";
+//    public static final String PARAM_PROD_DESC = "prodDesc";
+//    public static final String PARAM_TAX_EXEMPT_ID = "taxExemptID";
+//    public static final String PARAM_REF_NUMBER = "refNumber";
+//    public static final String PARAM_AUTH_CODE = "authCode";
+//    public static final String PARAM_RESPONSE_STATUS = "responseStatus";
     public static final String PARAM_EINPUT_TYPE = "eInputType";
-    public static final String PARAM_MAX_VALUE = "maxValue";
-    public static final String PARAM_MIN_VALUE = "minValue";
+//    public static final String PARAM_MAX_VALUE = "maxValue";
+//    public static final String PARAM_MIN_VALUE = "minValue";
     public static final String PARAM_MAX_LENGTH = "maxLength";
     public static final String PARAM_MIN_LENGTH = "minLength";
     public static final String PARAM_TIP_UNIT = "tipUnit";
@@ -145,11 +146,11 @@ public static final String PARAM_BALANCE = "balance";
      * PARAM_TRANS_STATUS : transaction status <br>
      * To Do : enum: upload, offline, voided, Adjusted ??????
      */
-    public static final String PARAM_TRANS_STATUS = "transStatus";
-    public static final String PARAM_AVS_RESPONSE = "avsResponse";
-    public static final String PARAM_ENTRY_MODE = "entryMode";
-    public static final String PARAM_FOOD_STAMPS_BALANCE = "foodStampsBalance";
-    public static final String PARAM_CASH_BENEFIT_BALANCE = "cashBenefitBalance";
+//    public static final String PARAM_TRANS_STATUS = "transStatus";
+//    public static final String PARAM_AVS_RESPONSE = "avsResponse";
+//    public static final String PARAM_ENTRY_MODE = "entryMode";
+//    public static final String PARAM_FOOD_STAMPS_BALANCE = "foodStampsBalance";
+//    public static final String PARAM_CASH_BENEFIT_BALANCE = "cashBenefitBalance";
 
     public static final String PARAM_RECEIPT_URI = "receiptUri";
     public static final String PARAM_EULA_URI = "eulaUri";
