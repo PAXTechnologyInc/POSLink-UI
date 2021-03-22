b5768bb5febbb174eefd1a3ae4b3b287c570284d
Jolie-Yanglj <jolie.yang@pax.us>
2021-03-22 15:53:05 -0400

    Add support new poslink command A06

---
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/PoslinkEntry.java
@@ -0,0 +1,31 @@
+package com.pax.us.pay.ui.constant.entry;
+
+public class PoslinkEntry {
+    public static final String CATEGORY = "com.pax.us.pay.ui.category.POSLINK";
+
+    /**
+     * The intent action of show dialog <br>
+     * input: {@link EntryExtraData#PARAM_PACKAGE} <br>
+     * input: {@link EntryExtraData#PARAM_MESSAGE} <br>
+     * input: {@link EntryExtraData#PARAM_TITLE} } <br>
+     * input: {@link EntryExtraData#PARAM_OPTIONS} <br>
+     * input: {@link EntryExtraData#PARAM_TIMEOUT} <br>
+     * input: {@link EntryExtraData#PARAM_CONTINUE_SCREEN} <br>
+     * request: {@link EntryRequest#PARAM_INDEX} <br>
+     */
+    public static final String ACTION_SHOW_DIALOG = "com.pax.us.pay.action.SHOW_DIALOG";
+
+
+    /**
+     * The intent action of show dialog <br>
+     * input: {@link EntryExtraData#PARAM_PACKAGE} <br>
+     * input: {@link EntryExtraData#PARAM_MESSAGE} <br>
+     * input: {@link EntryExtraData#PARAM_TITLE} } <br>
+     * input: {@link EntryExtraData#PARAM_MESSAGE_1} <br>
+     * input: {@link EntryExtraData#PARAM_MESSAGE_2} <br>
+     * input: {@link EntryExtraData#PARAM_TIMEOUT} <br>
+     * request: {@link EntryRequest#PARAM_INDEX} <br>
+     */
+    public static final String ACTION_SHOW_THANK_YOU = "com.pax.us.pay.action.SHOW_THANK_YOU";
+
+} 
---
+++ lib-ui-constant/src/main/java/com/pax/us/pay/ui/constant/entry/enumeration/ManageUIConst.java
@@ -0,0 +1,48 @@
+package com.pax.us.pay.ui.constant.entry.enumeration;
+
+/**
+ * Created by ZZ on 8/7/2020
+ */
+public class ManageUIConst {
+    public class ContinuousScreen {
+        public static final String DEFAULT = "0";
+        public static final String DO_NOT_GO_TO_IDLE = "1";
+    }
+
+    public class LineItemAction {
+        public static final String ADD = "0";
+        public static final String UPDATE = "1";
+        public static final String DELETE = "2";
+    }
+
+    public class InputType {
+        public static final String ALPHA_NUMERIC = "0";
+        public static final String NUMERIC = "1";
+        public static final String DATE = "2";
+        public static final String TIME = "3";
+        public static final String CURRENCY = "4";
+        public static final String PASSWORD = "5";
+        public static final String PHONE = "6";
+    }
+
+    public class HardKeys {
+        public static final String KEY_0 = "KEY0";
+        public static final String KEY_1 = "KEY1";
+        public static final String KEY_2 = "KEY2";
+        public static final String KEY_3 = "KEY3";
+        public static final String KEY_4 = "KEY4";
+        public static final String KEY_5 = "KEY5";
+        public static final String KEY_6 = "KEY6";
+        public static final String KEY_7 = "KEY7";
+        public static final String KEY_8 = "KEY8";
+        public static final String KEY_9 = "KEY9";
+        public static final String KEY_CANCEL = "KEYCANCEL";
+        public static final String KEY_ENTER = "KEYENTER";
+        public static final String KEY_CLEAR = "KEYCLEAR";
+    }
+
+    public class LabelProperty {
+        public static final String UNCHECKED = "0";
+        public static final String CHECKED = "1";
+    }
+}
