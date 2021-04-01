6edecf5d73792bc37072d85d5bbb3ea5520431f4
MichealUfl <anakinskywalker.cn@gmail.com>
2021-04-01 11:33:04 -0400

    Updated installment plan helper

--- lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/ConfirmInstallmentPlanHelper.java
+++ lib-ui-core/src/main/java/com/pax/us/pay/ui/core/helper/ConfirmInstallmentPlanHelper.java
@@ -3,6 +3,7 @@ package com.pax.us.pay.ui.core.helper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.pax.us.pay.ui.constant.entry.EntryRequest;
 import com.pax.us.pay.ui.core.BaseActionHelper;
 import com.pax.us.pay.ui.core.api.IRespStatus;
 import com.pax.us.pay.ui.core.api.IUIListener;
@@ -14,7 +15,7 @@ public class ConfirmInstallmentPlanHelper extends BaseActionHelper {
 
     public void sendNext(String planOption) {
         Bundle bundle = new Bundle();
-        bundle.putString("InstallmentOption", planOption);
+        bundle.putString(EntryRequest.INSTALLMEN_SELECT_OPTION, planOption);
         super.sendNext(bundle);
     }
 }
