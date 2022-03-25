6d8089b191705db8d7aceff81b1260b228ca2d1b
Jolie-Yanglj <jolie.yang@pax.us>
2022-03-25 17:34:25 -0400

    [BPOSANDJAX-270]Tip UI Enhancement

--- app/src/main/java/com/pax/pay/ui/def/EnterTipActivity.java
+++ app/src/main/java/com/pax/pay/ui/def/EnterTipActivity.java
@@ -5,8 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 
 import com.pax.pay.ui.def.base.RespStatusImpl;
 import com.pax.us.pay.ui.constant.entry.EntryExtraData;
